e4a4b3fe91e37b5465190b8d693332614c4fd0fc
Andy <andy.ruan2005@gmail.com>
2025-02-15 16:00:51 +1100

    made basic format for postcards

--- src/pages/Hobbies.jsx
+++ src/pages/Hobbies.jsx
@@ -1,15 +1,32 @@
 import React from "react";
 import "../styles/general.css";
 import "../styles/hobbies.css"
-
+import gamingImage from "../images/gaming.png"
+import aquarium from "../images/aquarium.jpg"
+import reading from "../images/reading.webp"
+import programming from "../images/programming.webp"
 
 function Hobbies() {
     return (
         <div className="hobbies-container">
-            <div className="postcard">Postcard 1</div>
-            <div className="postcard">Postcard 2</div>
-            <div className="postcard">Postcard 3</div>
-            <div className="postcard">Postcard 4</div>
+            <div className="postcard">
+                <img src={gamingImage}></img>
+                <div>
+                    <p>Gaming</p>
+                </div>
+            </div>
+            <div className="postcard">
+                <img src={aquarium}></img>
+                <p>Aquarist</p>
+            </div>
+            <div className="postcard">
+                <img src={reading}></img>
+                <p>Reading</p>
+            </div>
+            <div className="postcard">
+                <img src={programming}></img>
+                <p>Programming</p>
+            </div>
         </div>
     )
 }
