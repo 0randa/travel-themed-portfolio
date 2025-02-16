e42ac0a3d0cc0e6d980808af1054bb6f2cb87c12
Andy <andy.ruan2005@gmail.com>
2025-02-16 21:16:27 +1100

    finished skeleton for hobbies

--- src/pages/Hobbies.jsx
+++ src/pages/Hobbies.jsx
@@ -31,7 +31,9 @@ function Hobbies() {
                             <li>Stardew Valley</li>
                         </ul>
                     </div>
-                    <img className="stamp" src={ausStamp}></img>
+                    <div className="stamp-container">
+                        <img className="stamp" src={ausStamp}></img>
+                    </div>
                     
                 </div>
                 <div className="postcard">
@@ -39,26 +41,75 @@ function Hobbies() {
                     <div className="postcard-mid">
                         <p>Aquarist</p>
                         <p>Aquariums is a hobby where you raise your fish and manage your aquatic ecosystems.</p>
-                        <p>I have a large fish tank that is filled with large plants. My tank is inhabited by Guppies,
-                            and they live in an environment that is closely resembled to their natural habitat where
-                            they can breed and survive.
+                        <p>I have a large fish tank filled with live plants, creating a natural habitat for my guppies to thrive and breed.
                         </p>
                     </div>
-                    <img className="stamp" src={chinaStamp}></img>
+                    <div className="stamp-container">
+                        <img className="stamp" src={chinaStamp}></img>
+                    </div>
                 </div>
                 <div className="postcard">
                     <img src={reading}></img>
                     <div className="postcard-mid">
                         <p>Reading</p>
+
+                        <p>
+                            Favourite genres:
+                            <li>
+                                Fantasy
+                            </li>
+                            
+                            <li>
+                                Historical Fiction
+                            </li>
+
+                            <li>
+                                Nonfiction
+                            </li>
+
+                        </p>
+                        <p>
+                            Favourite books:
+                            <li>
+                                Harry Potter series
+                            </li>
+
+                            <li>
+                                A Life on Our Planet by David Attenborough
+                            </li>
+                        </p>
+
+                        <p>
+                            Currently reading:
+                            <li>
+                                Journey to the West
+                            </li>
+                        </p>
                     </div>
-                    <img className="stamp" src={japanStamp}></img>
+                    <div className="stamp-container">
+                        <img className="stamp" src={japanStamp}></img>
+                    </div>
                 </div>
                 <div className="postcard">
                     <img src={programming}></img>
                     <div className="postcard-mid">
                         <p>Programming</p>
+                        <p>
+                            As a CS student, it would come to no surprise that I have programming as a hobby xd.
+                            I am interested in high level programming languages such as Javascript and Python, due to
+                            their extensive libraries.
+                        </p>
+
+                        <p>
+                            As of creating this project, I am also learning react, after learning HTML and CSS, to enrich
+                            my understanding of frontend programming.
+                        </p>
+
+                        <p>Feel free to explore my personal projects by clicking "projects" on the navigation bar</p>
                     </div>
-                    <img className="stamp" src={franceStamp}></img>
+                    <div className="stamp-container">
+                        <img className="stamp" src={franceStamp}></img>
+                    </div>
                 </div>
             </div>
         </>
